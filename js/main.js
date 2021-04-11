2cef520c9cee29d57349223991130c3874b21ae5
lightbluecactus <violetday77@yahoo.com>
2021-04-11 12:45:57 -0400

    finish product page

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@
 
 			aboutBanner = document.querySelectorAll(".aboutBanner"),
 
-			sliderControl = document.querySelector("#sliderControl"),
+			sliderIcon = document.querySelectorAll(".sliderIcon"),
 			promoImg = document.querySelector("#promoImg"),
 			productName = document.querySelector("#productText h2"),
 			productDesc = document.querySelector("#productText p");
 
 	const productContent = [
 		[`Premium Beer`, `The classic taste of CoorS is only for you. Best served when refrigerated, or added ice into beer. 330mL 4.2%ALC/VOL.`],
-		[`6-Pack Beer`, `pack beer intro`],
-		[`Membership Card`, `member card intro`],
-		[`Tote Bag`, `bag intro`],
-		[`CoorS T-shirt`, `t-shirt intro`]
+		[`6-Pack Beer`, `If you want to share CoorS beer with your friends, the 6-pack will be a great choice. 330mL 4.2%ALC/VOL. each.`],
+		[`Membership Card`, `Joining CoorS community is more than just for discount. We also offer a custom fitness plan for you to enjoy sports with beer!`],
+		[`Tote Bag`, `Whenever we purchase at the counter, using an environment-friendly tote bag is always a responsible choice to reduce plastics.`],
+		[`CoorS T-shirt`, `Wearing a CoorS brand T-shirt is designed for outdoor sports. Lightweight and breathable, it helps you to get away from sweaty clothes.`]
 	];
 
 
@@ -33,25 +33,19 @@
 	}
 
 	function slideBanner(event) {
+		console.log(event);
 		let multiplier = event.target.dataset.offset;
-		if (event.target.style.width="40px") {			
-			offsetWidth = 262; 
-			profileImages.style.right = `${multiplier * offsetWidth}px`;
-		}
-
-		else {
-			offsetWidth = 450; 
-			profileImages.style.right = `${multiplier * offsetWidth}px`;
-		}
+		let offsetWidth = 554;
+		promoImg.style.right = `${multiplier * offsetWidth}px`;
 	}
 
 	function setProductData(name, desc) {
 		productName.textContent = name;
-		productDesc.textContent = text;
+		productDesc.textContent = desc;
 	}
 
 	function changeText(event) {
-	setMemberData(productContent[event.target.dataset.offset][0],
+	setProductData(productContent[event.target.dataset.offset][0],
 					productContent[event.target.dataset.offset][1]);
 	}
 
@@ -60,8 +54,8 @@
 
 	aboutBanner.forEach(banner => banner.addEventListener("click", expandText));
 
-	sliderControl.forEach(icon => icon.addEventListener("click", slideBanner));
-	sliderControl.forEach(icon => icon.addEventListener("click", changeText));
+	sliderIcon.forEach(icon => icon.addEventListener("click", slideBanner));
+	sliderIcon.forEach(slider => slider.addEventListener("click", changeText));
 
 
 
