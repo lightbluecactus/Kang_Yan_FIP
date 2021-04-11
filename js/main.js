5575b534c44fecff96c5232175cc9137b208bf63
lightbluecactus <violetday77@yahoo.com>
2021-04-10 21:19:57 -0400

    finish about page

--- js/main.js
+++ js/main.js
@@ -3,14 +3,26 @@
 	console.log("javascript is linked up");
 
 	const burgerBtn = document.querySelector("#burger"),
-			burgerCon = document.querySelector("#burgerCon");
+			burgerCon = document.querySelector("#burgerCon"),
+
+			aboutBanner = document.querySelectorAll(".aboutBanner");
+
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		burgerBtn.classList.toggle("expanded");
 	}
 
+	function expandText(event) {
+		console.log(event);
+		let currentBanner = event.target.dataset.about;
+		document.querySelector(`#${currentBanner}Text`).style.display = "block";
+	}
+
+
 	burgerBtn.addEventListener("click", hamburgerMenu, false);
+
+	aboutBanner.forEach(banner => banner.addEventListener("click", expandText));
 
 
 
